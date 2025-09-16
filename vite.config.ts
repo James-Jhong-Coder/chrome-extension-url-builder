ca3b25a43937d50788d3fd3231030335d821b151
JamesJhong <jamesjhong@star-link.tech>
2025-09-16 15:08:40 +0800

    feat: 🎸 加入 unplugin-vue-components setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: ['src/components/common'],
+      dts: 'src/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // 讓 @ 指向 src
